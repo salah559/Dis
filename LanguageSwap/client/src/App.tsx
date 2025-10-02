6eff9c0d9611786c406268666b475abeb87ac615
salah559 <142224356+salah559@users.noreply.github.com>
2025-10-03 00:09:29 +0100

    رفع ملفات من ZIP

---
+++ LanguageSwap/client/src/App.tsx
@@ -0,0 +1,38 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { LanguageProvider } from "@/hooks/useLanguage";
+import Home from "@/pages/home";
+import About from "@/pages/about";
+import Projects from "@/pages/projects";
+import Contact from "@/pages/contact";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <LanguageProvider>
+          <Toaster />
+          <Router />
+        </LanguageProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
